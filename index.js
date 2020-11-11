c31f0c0723c20fd3cf72f8047f804f3bff3edf94
António Eloi <aeloi@student.dei.uc.pt>
2020-11-11 22:47:30 +0000

    Add first part create web app

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Logic Here
+// Import url to fetch da articles
+import { newsUrl } from './newsApi.js';
+// Article element class description
+import './news-article.js';
+
+window.addEventListener('load', () => {
+    getPWANews(); 
+});
+
+// Method to retrieve articles from API (async)
+async function getPWANews() {
+    // GET request to articles url
+    const res = await fetch(newsUrl);
+    // Result json
+    const json = await res.json();
+
+    // Now that we ahve the json response we can add it to the page
+    const main = document.querySelector('main');
+    json.articles.forEach(article => {
+        const el = document.createElement('news-article');
+        el.article = article;
+        main.appendChild(el);
+    });
+}
